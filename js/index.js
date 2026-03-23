09a13072e0b63d8dbea0cc86f845f4e8c0cf1623
Adam thor <adabelrie@alu.edu.gva.es>
2026-03-23 16:30:16 +0100

    cambios

--- js/index.js
+++ js/index.js
@@ -1,26 +1,36 @@
+window.onload = inicio;
 
-window.onload=inicio;
-let usuario= {
-    dni:"20942599V",
-    nom:"Adam Thor Bellver Riera",
-    email:"adamthor@gmail.com"
-
+let usuario = {
+    dni: "20942599V",
+    nom: "Adam Thor Bellver Riera",
+    email: "adamthor@gmail.com"
 };
+
 function inicio(){
-ValidarDatos(usuario);
+    Pedirdatos();
+    ValidarDatos(usuario);
 }
+
 function Pedirdatos(){
-usuario.dni=prompt("Introduce el dni del cliente");
-usuario.nom=prompt("Introduce el nombre del cliente");
-usuario.email=prompt("Introduce el email del cliente");
+    usuario.dni = prompt("Introduce el dni del cliente");
+    usuario.nom = prompt("Introduce el nombre del cliente");
+    usuario.email = prompt("Introduce el email del cliente");
 }
 
 function ValidarDatos(usuario){
-let errores=[];
-if(usuario.nom.slipt("").length!==2){
-    errores.push("el nombre del cliente tiene que tener 2 palabras");
-}
-if (!/^\d{8}[a-zA-Z]$/.test(usuario.dni)){
-       errores.push("el dni ha de tenir 8 números i una lletra al final");
-}
+    let errores = [];
+
+    if(usuario.nom.trim().split(" ").length < 2){
+        errores.push("El nombre del cliente tiene que tener al menos 2 palabras");
+    }
+
+    if (!/^\d{8}[a-zA-Z]$/.test(usuario.dni)){
+        errores.push("El dni ha de tener 8 números y una letra al final");
+    }
+
+    if (errores.length > 0){
+        alert(errores.join("\n"));
+    } else {
+        alert("Datos correctos");
+    }
 }
