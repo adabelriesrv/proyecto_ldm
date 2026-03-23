3ab77c25c459cff2e60eb34a8cf3dc5894c6cea8
Adam thor <adabelrie@alu.edu.gva.es>
2026-03-23 16:05:37 +0100

    pedirdatos

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+window.onload=inicio;
+
+function inicio(){
+pedirDatos();
+}
+function pedirDatos(){
+
+
+var ArrayDatos= new Array();
+ArrayDatos['dni']- prompt("Introduce Dni");
+ArrayDatos['nombre']=prompt("Introduce Nombre");
+ArrayDatos['correo']=prompt("Introduce Correo");
+}
