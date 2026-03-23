272d95a257a25862459a100e0210848bdf47800f
Adam thor <adabelrie@alu.edu.gva.es>
2026-03-23 16:16:26 +0100

    validardatosincomple

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 
 window.onload=inicio;
+let usuario= {
+    dni:"20942599V",
+    nom:"Adam Thor Bellver Riera",
+    email:"adamthor@gmail.com"
 
+};
 function inicio(){
 pedirDatos();
 }
 function pedirDatos(){
 
 
-var ArrayDatos= new Array();
-ArrayDatos['dni']- prompt("Introduce Dni");
-ArrayDatos['nombre']=prompt("Introduce Nombre");
-ArrayDatos['correo']=prompt("Introduce Correo");
+
+
+}
+function ValidarDatos(usuario){
+let errores=[];
+if(usuario.nom.slipt("").length !==2){
+    errores.push("el nombre del cliente tiene que tener 2 palabras");
+}
+if (!/^\d{8}[a-zA-Z]$/.test(usuario.dni)){
+       errores.push("el dni ha de tenir 8 números i una lletra al final");
+}
 }
